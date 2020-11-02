1652eabedb4e928c221705667c0a84050bc3b35a
Liam <liam.heuwagen@eurosoft.net>
2020-11-02 08:23:50 +0100

    add more comment stuff

---
+++ src/Controller/CommentAdminController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CommentRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CommentAdminController extends Controller
+{
+    /**
+     * @Route("/admin/comment", name="comment_admin")
+     * @param CommentRepository $repository
+     * @param Request $request
+     * @return Response
+     */
+    public function index(CommentRepository $repository, Request $request)
+    {
+        $q = $request->query->get('q');
+
+        $comments = $repository->findAllWithSearch($q);
+
+        return $this->render('comment_admin/index.html.twig', [
+            'comments' => $comments,
+        ]);
+    }
+
+
+}
