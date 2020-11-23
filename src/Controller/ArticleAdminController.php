55d8be19a69fe20071d8b4572856c2ded117646c
Liam <liam.heuwagen@eurosoft.net>
2020-11-23 14:57:48 +0100

    close some security gaps and end the course

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -7,13 +7,12 @@ use App\Form\ArticleFormType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleAdminController extends AbstractController
+class ArticleAdminController extends BaseController
 {
     /**
      * @Route("/admin/article/new", name="admin_article_new")
@@ -82,9 +81,14 @@ class ArticleAdminController extends AbstractController
      * @Route("/admin/article/location-select", name="admin_article_location_select")
      * @param Request $request
      * @return Response
+     * @IsGranted("ROLE_USER")
      */
     public function getSpecificLocationSelect(Request $request)
     {
+        if(!$this->isGranted('ROLE_ADMIN_ARTICLE') && $this->getUser()->getArticles()->isEmpty()){
+            throw $this->createAccessDeniedException();
+        }
+
         $article = new Article;
         $article->setLocation($request->query->get('location'));
         $form = $this->createForm(ArticleFormType::class, $article);
