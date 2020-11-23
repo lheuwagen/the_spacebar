83d8f4edd814579eb65891a7b610003878101aec
Liam <liam.heuwagen@eurosoft.net>
2020-11-23 14:22:23 +0100

    add internet magix to change location selection on edit and new

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -68,12 +68,33 @@ class ArticleAdminController extends AbstractController
 
             $this->addFlash('success', 'Article updated! Knowledge is power!');
 
-            return $this->redirectToRoute('admin_article_edit',[
+            return $this->redirectToRoute('admin_article_edit', [
                 'id' => $article->getId(),
             ]);
         }
 
         return $this->render('article_admin/edit.html.twig', [
+            'articleForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/article/location-select", name="admin_article_location_select")
+     * @param Request $request
+     * @return Response
+     */
+    public function getSpecificLocationSelect(Request $request)
+    {
+        $article = new Article;
+        $article->setLocation($request->query->get('location'));
+        $form = $this->createForm(ArticleFormType::class, $article);
+
+        // no field? Return an empty response
+        if (!$form->has('specificLocationName')) {
+            return new Response(null, 204);
+        }
+
+        return $this->render('article_admin/_specific_location_name.html.twig', [
             'articleForm' => $form->createView(),
         ]);
     }
